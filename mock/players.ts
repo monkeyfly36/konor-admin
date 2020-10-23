7cae1cb2e6637aa586e9d7c2c6ab80c611d25b58
zhifeihe <zhifeihe@deepglint.com>
2020-10-23 19:23:02 +0800

    finish players api

---
+++ mock/players.ts
@@ -0,0 +1,114 @@
+import { Response, Request } from 'express'
+import { Hero, Player } from '../src/api/types'
+import { heros } from './heros'
+import faker from 'faker'
+
+faker.locale = 'zh_CN'
+
+// 模拟玩家数据
+const playerCount = 100
+const playerList: Player[] = []
+
+for (let i = 0; i < playerCount; i++) {
+  playerList.push({
+    id: i,
+    acountname: faker.name.findName(),
+    avatar: faker.image.avatar(),
+    bravepoints: faker.random.number(1000),
+    exp: faker.random.number(10000),
+    level: faker.random.number(30),
+    nickname: faker.name.findName(),
+    rank: faker.random.number(200),
+    wanttoplay: genWanttoplay(),
+    winningstreak: faker.random.number(10),
+  })
+}
+
+function genWanttoplay() {
+  let wanttoplay: Set<Hero> = new Set()
+  while(wanttoplay.size < 3) {
+    wanttoplay.add(heros[faker.random.number(9)])
+  }
+  return Array.from(wanttoplay)
+}
+
+// 路由实现--获取玩家信息
+export const getPlayers = (req: Request, res: Response) => {
+  // 从查询参数中获取分页，过滤关键词等参数
+  const { accountname, page = 1, limit = 10 } = req.query
+  // 过滤
+  let mockList = playerList.filter(item => {
+    if (accountname && item.acountname.indexOf(<string>accountname) < 0) {
+      return false
+    }
+    return true
+  })
+  // 分页
+  const pageList = mockList.filter((item, index) => index < <number>limit * <number>page && index >= <number>limit * (<number>page - 1))
+  res.json({
+    code: 200000,
+    data: {
+      total: mockList.length,
+      players: pageList
+    }
+  })
+}
+// 查询玩家信息
+export const getPlayer = (req: Request, res: Response) => {
+  const { id } = req.params
+  for (const player of playerList) {
+    if(player.id.toString() === id) {
+      res.json({
+        code: 200000,
+        data: {
+          player
+        }
+      })
+    }
+  }
+  // 没有找到数据，报错
+  res.json({
+    code: 701000,
+    message: '没有找到对应的玩家'
+  })
+}
+// 创建玩家
+export const createPlayer = (req: Request, res: Response) => {
+  // 新增的英雄数据
+  const { player } = req.body
+  // 直接返回
+  res.json({
+    code: 200000,
+    data: {
+      player
+    }
+  })
+}
+// 更新玩家
+export const updatePlayer = (req: Request, res: Response) => {
+  // 新增的英雄数据
+  const { id } = req.params
+  const { params } = req.body
+  for (const player of playerList) {
+    if(player.id.toString() === id) {
+      res.json({
+        code: 200000,
+        data: {
+          player
+        }
+      })
+    }
+  }
+  // 直接返回
+  res.json({
+    code: 70001,
+    message: '没有找到响应玩家信息'
+  })
+}
+// 删除玩家
+export const deletePlayer = (req: Request, res: Response) => {
+  // 直接返回
+  res.json({
+    code: 20000
+  })
+}
--- src/api/types.d.ts
+++ src/api/types.d.ts
@@ -42,3 +42,25 @@ export interface IUserData {
   introduction: string
   roles: string[]
 }
+
+// 英雄数据结构
+export interface Hero {
+  id: number
+  name: string // 英雄名称
+  icon: string // 英雄头像图标
+  classify: string[] // 英雄分类
+}
+
+// 玩家数据结构
+export interface Player {
+  id: number,
+  acountname: string, // 账户名
+  nickname: string, // 昵称
+  avatar: string, // 用户头像
+  level: number, // 用户等级
+  exp: number, // 用户经验值
+  rank: number, // 排位赛段位
+  bravepoints: number, // 勇者积分
+  winningstreak: number, // 连胜场次
+  wanttoplay: Hero[] // 想玩英雄
+}
